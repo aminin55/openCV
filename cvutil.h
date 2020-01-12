be8de189b01d33c9ae77c4f518d535780d707650
aminin55 <43518781+aminin55@users.noreply.github.com>
2020-01-12 23:26:39 +0300

    Add files via upload

--- cvutil.h
+++ cvutil.h
@@ -3,15 +3,36 @@
 #include <string>
 #include <iostream>
 #include <cctype>
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+#include <Windows.h>
+#include <opencv2/opencv.hpp>
+#include <utility>
+
+#define SET_PARAM_INT(param) \
+			if (kv.count(#param))\
+				 params.param = (int)kv.at(#param);\
+			 else \
+				throw runtime_error(string("Can't find parameter ") + string(#param) + string(" in ") + filename)
+#define SET_PARAM_DOUBLE(param) \
+			if (kv.count(#param))\
+				 params.param = kv.at(#param);\
+			 else \
+				throw runtime_error("Can't find parameter param in " + filename)
+
+
 using namespace std;
 
 struct parameters {
 	int num_x = 1;	// Кол-во знакомест по горизонтали             
 	int num_y = 2;	// Кол-во знакомест по вертикали               
-	double delta_x = 21.5; // Ширина одного знакоместа             
+	double delta_x = 21.5; // Ширина одного знакоместа
 	double delta_y = 33.0; // Длина одного знакоместа                   
 	int start_x = 28; // Откуда строим линиии по вертикали         
 	int start_y = 23; // Откуда строим линии по горизонтали        
+	vector<string> names_int = { "start_x","start_y","num_x","num_y" };
+	vector<string> names_double = { "delta_x","delta_y"};
+
 	vector<vector<bool>> positions;
 };
 
@@ -21,20 +42,19 @@ void clear_spaces(string& s) {
 
 bool parse_config(parameters& params, string filename) {
 	map<string, double> kv;
-	filename = filename.substr(0, filename.find(".")) + ".cfg";
+	filename = filename.substr(0, filename.find(".")) + ".cfg"; // На вход пришло имя видео-файла. Заменяем его на file.cfg
 	ifstream fin(filename.c_str());
-	if (!fin.is_open()) {
-		cerr << "Can't open " << filename << endl;
-		return false;
-	}
+	
+	if (!fin.is_open()) throw runtime_error("Can't open " + filename); 	// Если файл не открылся - кидаем exceprion
+	
 	std::string line;
 	while (getline(fin, line)) {
-		cout << endl << "$debug";
+		//cout << endl << "$debug";
 		clear_spaces(line);
 		if (line[0] != '/' && !isdigit(line[0]) && line.size() > 1) {
 			string key = line.substr(0, line.find("="));
 			string value = line.substr(line.find("=") + 1);
-			cout << '"' << key << "\" \"" << value << '"' << endl;
+			//cout << '"' << key << "\" \"" << value << '"' << endl;
 			kv[key] = stod(value);
 		}
 		else if (line[0] == '0' || line[0] == '1') {
@@ -42,51 +62,73 @@ bool parse_config(parameters& params, string filename) {
 			for (auto s : line) {
 				if (s == '0') temp.push_back(false);
 				else if (s == '1') temp.push_back(true);
-				else {
-					cout << "Parse config error. Waiting for 0 or 1. Got" << s << endl;
-					return false;
-				}
-				cout << temp.back();
+				
+				else throw runtime_error ("Parse config error. Waiting for 0 or 1. Got "+ s);
+				
+				//cout << temp.back();
 			}
 			params.positions.push_back(temp);
 		}
 	}
 	fin.close();
-	if (kv.count("delta_x"))
-		params.delta_x = kv.at("delta_x");
-	else
-		return false;
-	if (kv.count("delta_y"))
-		params.delta_y = kv.at("delta_y");
-	else
-		return false;
-	if (kv.count("start_x"))
-		params.start_x = (int)kv.at("start_x");
-	else
-		return false;
-	if (kv.count("start_y"))
-		params.start_y = (int)kv.at("start_y");
-	else
-		return false;
-	if (kv.count("num_x"))
-		params.num_x = (int)kv.at("num_x");
-	else
-		return false;
-	if (kv.count("num_y"))
-		params.num_y = (int)kv.at("num_y");
-	else
-		return false;
-	if ((int)params.positions.size() != params.num_y) {
-		cout << params.positions.size() << " != " << params.num_y << endl;
-		return false;
-	}
+	
+
+	SET_PARAM_DOUBLE(delta_x);
+	SET_PARAM_DOUBLE(delta_y);
+	SET_PARAM_INT(start_x);
+	SET_PARAM_INT(start_y);
+	SET_PARAM_INT(num_x);
+	SET_PARAM_INT(num_y);
+
+
+	if ((int)params.positions.size() != params.num_y) 
+		throw runtime_error("Parse config error. num_y is " + to_string(params.positions.size()) + " ,but must be " + to_string(params.num_y));
+	
 
 	for (auto v : params.positions) {
-		if ((int)v.size() != (int)params.num_x) {
-			cout << v.size() << " != " << (int)params.num_x << endl;
-			return false;
-		}
+		if ((int)v.size() != (int)params.num_x) 
+			throw runtime_error("Parse config error. num_x is " + to_string(v.size()) + " ,but must be " + to_string(params.num_x)); 
 	}
 	return true;
 }
 
+void draw_greed(parameters params, cv::Mat frame, const cv::Scalar Color) {
+	for (int i = 0; i < params.num_y; i++) {
+		for (int j = 0; j < params.num_x; j++) {
+			cv::Point tl((int)(params.start_x + params.delta_x * j), (int)(params.start_y + params.delta_y * i));											// Top / Left
+			cv::Point br((int)(params.start_x + params.delta_x * (j + 1)), (int)(params.start_y + params.delta_y * (i + 1)));									// Bottom / Right
+			if (params.positions.at(i).at(j)) {
+				cv::rectangle(frame, tl, br, Color, 1, 0);
+			}
+		}
+	}
+}
+
+pair<char,char> UTF2OEM(char* src = NULL) {
+	int length = MultiByteToWideChar(CP_UTF8, 0, (LPCSTR)src, -1, NULL, 0);
+	if (length > 0)
+	{
+		wchar_t* wide = new wchar_t[length];
+		MultiByteToWideChar(CP_UTF8, 0, (LPCSTR)src, -1, wide, length);
+		// convert it to ANSI
+		char* ansi = new char[length];
+		char* ansi2 = new char[length];
+		WideCharToMultiByte(CP_OEMCP, 0, wide, -1, ansi, length, NULL, NULL);
+		WideCharToMultiByte(CP_ACP, 0, wide, -1, ansi2, length, NULL, NULL);
+		//delete[] wide;
+		return make_pair(ansi[0], ansi2[0]);
+	}
+	return make_pair(NULL, NULL);;
+}
+
+pair<char,char> recog_char(tesseract::TessBaseAPI* api,parameters params,cv::Mat dst, int x,int y) {
+	//			api->TesseractRect(dst.data, dst.channels(), dst.step1(), 26, 26, 20, 24);
+	api->TesseractRect(dst.data, dst.channels(), (int)dst.step1(), params.start_x+ (int)params.delta_x * x , 
+																   params.start_y+ (int)params.delta_y * y ,
+															       (int)params.delta_x, (int)params.delta_y);
+	api->Recognize(0);
+	char* outText;
+	outText = api->GetUTF8Text();
+	// convert multibyte UTF-8 to wide string UTF-16
+	return UTF2OEM(outText);
+}
